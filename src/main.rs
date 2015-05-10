4d0bd35f0705c55100989b9769db4098ea98ae89
Sean Hagar <rcix2010@gmail.com>
2015-05-10 15:09:42 -0700

    added code that demonstrates regex replace_all error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+
+extern crate regex;
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let source : String = String::from_str("itado");
+    let replace : String = String::from_str("t$1t");
+    let regex : Regex = match Regex::new(&"t(.)d") {
+        Ok(res) => res,
+        Err(err) => panic!("Error '{}' with regex '{}'", err, &"t(.)d")
+    };
+    let value = match regex.is_match(&source) {
+        true => regex.replace_all(&source, &replace),
+        false => String::from_str("none"),
+    };
+
 }
